ae16060d1404525fb6a1cf4398563982416498e9
ashokkondapuram <ashok.k14@outlook.com>
2026-06-24 20:13:46 -0400

    Update template index and gallery to include all 20 workflows

---
+++ frontend/src/templates/index.ts
@@ -0,0 +1,43 @@
+import wf_daily_report from "./wf_daily_report.json";
+import wf_db_backup from "./wf_db_backup.json";
+import wf_expense_report from "./wf_expense_report.json";
+import wf_invoice_approval from "./wf_invoice_approval.json";
+import wf_lead_nurture from "./wf_lead_nurture.json";
+import wf_new_employee from "./wf_new_employee.json";
+import wf_order_fulfillment from "./wf_order_fulfillment.json";
+import wf_slack_alert from "./wf_slack_alert.json";
+import wf_social_post from "./wf_social_post.json";
+import wf_support_ticket from "./wf_support_ticket.json";
+import wf_content_moderation from "./wf_content_moderation.json";
+import wf_contract_renewal from "./wf_contract_renewal.json";
+import wf_customer_churn from "./wf_customer_churn.json";
+import wf_employee_offboarding from "./wf_employee_offboarding.json";
+import wf_interview_scheduling from "./wf_interview_scheduling.json";
+import wf_product_restock from "./wf_product_restock.json";
+import wf_sales_pipeline from "./wf_sales_pipeline.json";
+import wf_security_incident from "./wf_security_incident.json";
+import wf_server_health from "./wf_server_health.json";
+import wf_subscription_billing from "./wf_subscription_billing.json";
+
+export const TEMPLATES = [
+  wf_daily_report,
+  wf_db_backup,
+  wf_expense_report,
+  wf_invoice_approval,
+  wf_lead_nurture,
+  wf_new_employee,
+  wf_order_fulfillment,
+  wf_slack_alert,
+  wf_social_post,
+  wf_support_ticket,
+  wf_content_moderation,
+  wf_contract_renewal,
+  wf_customer_churn,
+  wf_employee_offboarding,
+  wf_interview_scheduling,
+  wf_product_restock,
+  wf_sales_pipeline,
+  wf_security_incident,
+  wf_server_health,
+  wf_subscription_billing,
+];
